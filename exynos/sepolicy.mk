ffe0dccf05ae2c963fe6ab7cc4f509f58af54933
CannedShroud <hypersync112@gmail.com>
2022-05-30 19:21:08 +0530

    Init Lighthouse

--- exynos/sepolicy.mk
+++ exynos/sepolicy.mk
@@ -10,14 +10,14 @@ endif
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/custom/sepolicy/exynos/private
+    device/lighthouse/sepolicy/exynos/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/custom/sepolicy/exynos/dynamic \
-    device/custom/sepolicy/exynos/system
+    device/lighthouse/sepolicy/exynos/dynamic \
+    device/lighthouse/sepolicy/exynos/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/custom/sepolicy/exynos/dynamic \
-    device/custom/sepolicy/exynos/vendor
+    device/lighthouse/sepolicy/exynos/dynamic \
+    device/lighthouse/sepolicy/exynos/vendor
 endif
